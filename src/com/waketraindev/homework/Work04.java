38210e1a2f63425958a788620928adee71e97ac4
WakeTrainDev <175499942+waketraindev@users.noreply.github.com>
2024-08-04 17:58:39 +0300

    Commit Homework 4: 4. Simple Input/Output: Write a program that asks the user to enter two numbers, performs addition on those numbers, and prints out the result.
    (cherry picked from commit a189e871e970e3b5ff0abaac318d88a0433a37d9)
    (cherry picked from commit 77e4b7cb3863671a8cfb46fb7ce0e26ba819605c)

---
+++ src/com/waketraindev/homework/Work04.java
@@ -0,0 +1,26 @@
+package com.waketraindev.homework;
+
+/*
+ * Description:
+ * 4. Simple Input/Output: Write a program that asks the user to enter two numbers, performs addition on those numbers,
+ * and prints out the result.
+ */
+
+import java.util.Scanner;
+
+public class Work04 {
+    public static void main(String[] args) {
+        Scanner inputScanner = new Scanner(System.in);
+        System.out.print("Age (years): ");
+        final int age = inputScanner.nextInt();
+        System.out.print("Height (centimeters): ");
+        final int height = inputScanner.nextInt();
+        System.out.print("Weight (kilograms): ");
+        final double weight = inputScanner.nextDouble();
+
+        final int maxHeartRate = 220 - age;
+        final double bmi = (weight / height / height * 10_000);
+        System.out.printf("BMI: %.2f, Max Heart Rate: %d", bmi, maxHeartRate);
+
+    }
+}
